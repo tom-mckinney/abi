4152e59b4d02f438e0795dc24fa74bc7bbac27cb
Tom McKinney <tmckinney@jhancock.com>
2019-07-26 10:58:28 -0400

    WIP added property map assert

---
+++ src/Abi.Test/Exceptions/PropertyMapException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xunit.Sdk
+{
+    public class PropertyMapException : XunitException
+    {
+        public PropertyMapException(string message, Type originType, Type mapType) : this(message, originType, mapType, null)
+        {
+        }
+
+        public PropertyMapException(string message, Type originType, Type mapType, Exception innerException)
+            : base(FormatMessage(message, originType, mapType), innerException)
+        {
+        }
+
+        private static string FormatMessage(string message, Type originType, Type mapType)
+        {
+            return $"{message}\nAttempted to map from {originType?.Name} to {mapType?.Name}.";
+        }
+    }
+}
--- src/Abi.Test/Extensions/AssertExtensions.cs
+++ src/Abi.Test/Extensions/AssertExtensions.cs
@@ -1,15 +1,87 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 using Xunit;
+using Xunit.Sdk;
 
 namespace Abi.Test.Extensions
 {
     public static class AssertExtensions
     {
-        public static void AllPropertiesMapped(this Assert assert)
+        /// <summary>
+        /// Asserts that all properties of <paramref name="actual"/> have been mapped from <paramref name="expected"/>.
+        /// </summary>
+        /// <typeparam name="TOrigin">origin object type</typeparam>
+        /// <typeparam name="TMap">mapped object type</typeparam>
+        /// <param name="expected">origin object</param>
+        /// <param name="actual">mapped object</param>
+        /// <returns></returns>
+        public static bool AllPropertiesMapped<TOrigin, TMap>(TOrigin expected, TMap actual)
         {
+            return AllPropertiesMapped(expected, actual, new List<string>(), new Dictionary<string, string>());
+        }
 
+        /// <summary>
+        /// Asserts that all properties of <paramref name="actual"/> have been mapped from <paramref name="expected"/>.
+        /// All <paramref name="excludedProperties"/> will be excluded.
+        /// </summary>
+        /// <typeparam name="TOrigin">origin object type</typeparam>
+        /// <typeparam name="TMap">mapped object type</typeparam>
+        /// <param name="expected">origin object</param>
+        /// <param name="actual">mapped object</param>
+        /// <param name="excludedProperties">excluded properties</param>
+        /// <returns></returns>
+        public static bool AllPropertiesMapped<TOrigin, TMap>(TOrigin expected, TMap actual, IEnumerable<string> excludedProperties)
+        {
+            return AllPropertiesMapped(expected, actual, excludedProperties, new Dictionary<string, string>());
+
+        }
+
+        /// <summary>
+        /// Asserts that all properties of <paramref name="actual"/> have been mapped from <paramref name="expected"/>.
+        /// All <paramref name="excludedProperties"/> will be excluded and any properties with a matching key in <paramref name="translatedProperties"/> will be translated to respective property in class <typeparamref name="TOrigin"/>
+        /// </summary>
+        /// <typeparam name="TOrigin">origin object type</typeparam>
+        /// <typeparam name="TMap">mapped object type</typeparam>
+        /// <param name="expected">origin object</param>
+        /// <param name="actual">mapped object</param>
+        /// <param name="excludedProperties">excluded properties</param>
+        /// <param name="translatedProperties">translated properties</param>
+        /// <returns>True or throws Xunit exception</returns>
+        public static bool AllPropertiesMapped<TOrigin, TMap>(TOrigin expected, TMap actual, IEnumerable<string> excludedProperties, IDictionary<string, string> translatedProperties)
+        {
+            if (excludedProperties == null)
+                throw new ArgumentException("Argument cannot be null", nameof(excludedProperties));
+            if (translatedProperties == null)
+                throw new ArgumentException("Argument cannot be null", nameof(translatedProperties));
+
+            PropertyInfo[] originProperties = typeof(TOrigin).GetProperties();
+            PropertyInfo[] mappedProperties = typeof(TMap).GetProperties();
+
+            foreach (var prop in mappedProperties)
+            {
+                if (excludedProperties.Contains(prop.Name))
+                    continue;
+
+                if (!translatedProperties.TryGetValue(prop.Name, out string propertyName))
+                {
+                    propertyName = prop.Name;
+                }
+
+                try
+                {
+                    var originProperty = Assert.Single(originProperties, p => p.Name == propertyName);
+                    Assert.Equal(originProperty.GetValue(expected), prop.GetValue(actual));
+                }
+                catch (Exception e)
+                {
+                    throw new PropertyMapException($"Could not find matching property or value for {propertyName}.", typeof(TOrigin), typeof(TMap), e);
+                }
+            }
+
+            return true;
         }
     }
 }
