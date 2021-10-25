6f9b328bf9c2c3e873bc272e70bd0652a73a91c7
Tom McKinney <tom@upstreamcode.com>
2021-10-25 19:54:06 -0400

    WIP

--- src/Abi/Models/Experiment.cs
+++ src/Abi/Models/Experiment.cs
@@ -8,7 +8,7 @@ namespace Abi.Models
     {
         public string ExperimentId { get; set; } = null!;
         public string Name { get; set; } = null!;
-        public string ContentItemId { get; set; } = null!
+        public string ContentItemId { get; set; } = null!;
     }
 
     public class Experiment : Experiment<int>
